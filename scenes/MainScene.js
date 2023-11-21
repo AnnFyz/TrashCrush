8c3997e863412edf54161c32eb109ed275f34034
Anna <annfyz1232@yandex.ru>
2023-11-21 23:03:18 +0100

    updating collections and items

--- scenes/MainScene.js
+++ scenes/MainScene.js
@@ -3,82 +3,109 @@ const amountOfItems_biowaste = 3;
 let plasticItemCollection = [];
 let biowasteItemCollection = [];
 let index = 0;
+let amountOfCollections;
 const typeOfWaste = {
-    Plastic: 'plastic',
-    Paper: 'paper',
-    Biowaste: 'biowaste',
-    ResidualWaste: 'residualWaste',
-    Glass: 'glass'
+  Plastic: "plastic",
+  Paper: "paper",
+  Biowaste: "biowaste",
+  ResidualWaste: "residualWaste",
+  Glass: "glass",
 };
-
 
 let currentItemsCollection = []; //current collection for current level
 const itemCollections = [[]]; //all item collections
 
-// in collection will be stored items 
+// in collection will be stored items
 function fillItemCollection(gameThis, amountOfItems, wasteType) {
-    let itemCollection = [];
-    for (let i = 0; i < amountOfItems; i++) {
-        let item = new Item({ scene: gameThis, x: game.config.width / 2, y: game.config.height / 2, key: wasteType + i, index: i, wasteType });
-        itemCollection.push(item);
-    }
-    return itemCollection;
-
+  let itemCollection = [];
+  for (let i = 0; i < amountOfItems; i++) {
+    let item = new Item({ scene: gameThis, x: game.config.width / 2, y: game.config.height / 2, key: wasteType + i, index: i, wasteType });
+    itemCollection.push(item);
+  }
+  return itemCollection;
 }
 
 function fillAllCollections(gameThis) {
-    plasticItemCollection = fillItemCollection(gameThis, amountOfItems_plastic, typeOfWaste.Plastic).slice();
-    biowasteItemCollection = fillItemCollection(gameThis, amountOfItems_biowaste, typeOfWaste.Biowaste).slice();
-    itemCollections.push(plasticItemCollection);
-    itemCollections.push(biowasteItemCollection);
+  plasticItemCollection = fillItemCollection(gameThis, amountOfItems_plastic, typeOfWaste.Plastic).slice();
+  biowasteItemCollection = fillItemCollection(gameThis, amountOfItems_biowaste, typeOfWaste.Biowaste).slice();
+  itemCollections.push(plasticItemCollection);
+  itemCollections.push(biowasteItemCollection);
+  itemCollections.shift();
+  amountOfCollections = itemCollections.length;
 }
 
 class MainScene extends Phaser.Scene {
-
-    constructor() {
-        super('MainScene');
-    }
-    preload() {
-        for (let i = 0; i < amountOfItems_plastic; i++) {
-            this.load.image(typeOfWaste.Plastic + i, 'assets/images/Plastic/' + typeOfWaste.Plastic + i + '.png');
-        }
-
-        for (let i = 0; i < amountOfItems_biowaste; i++) {
-            this.load.image(typeOfWaste.Biowaste + i, 'assets/images/Biowaste/' + typeOfWaste.Biowaste + i + '.png');
-        }
+  constructor() {
+    super("MainScene");
+  }
+  preload() {
+    for (let i = 0; i < amountOfItems_plastic; i++) {
+      this.load.image(typeOfWaste.Plastic + i, "assets/images/Plastic/" + typeOfWaste.Plastic + i + ".png");
     }
 
-    create() {
-        this.emitter = EventDispatcher.getInstance();
-        fillAllCollections(this);
-        currentItemsCollection = biowasteItemCollection.slice();
-        this.activateFirstItem();
-        this.emitter.on(cons.ITEM_UPDATED, this.updateItem);
-
+    for (let i = 0; i < amountOfItems_biowaste; i++) {
+      this.load.image(typeOfWaste.Biowaste + i, "assets/images/Biowaste/" + typeOfWaste.Biowaste + i + ".png");
     }
+  }
 
-    update() {
-
+  create() {
+    this.emitter = EventDispatcher.getInstance();
+    this.sb = new Scorebox({ scene: this });
+    this.sb.x = game.config.width / 2;
+    this.sb.y = 45;
+    fillAllCollections(this);
+    this.updateCurrentCollection();
+    for (let i = 0; i < currentItemsCollection.length; i++) {
+      console.log("currentItemsCollection[0][0].wasteType " + currentItemsCollection[0].wasteType);
     }
+    this.activateItem();
+    this.emitter.on(cons.ITEM_UPDATED, this.updateItem.bind(this));
+  }
 
-    activateFirstItem() {
-        this.currentActiveItem = currentItemsCollection[0];
-        Align.scaleToGameW(this.currentActiveItem, .25);
-        this.currentActiveItem.activateItem();
-    }
-    updateItem() {
-        if (index < currentItemsCollection.length - 1) {
-            index++;
-            this.currentActiveItem = currentItemsCollection[index];
-            Align.scaleToGameW(this.currentActiveItem, .25);
-            this.currentActiveItem.activateItem();
-        }
+  update() {}
 
-        else {
-            console.log('the end');
-        }
+  activateItem() {
+    let tempItemCol = shuffle(currentItemsCollection);
+    this.currentActiveItem = tempItemCol[0];
+    Align.scaleToGameW(this.currentActiveItem, 0.25);
+    this.currentActiveItem.activateItem();
+  }
 
-    }
+  updateItem() {
+    this.removeShowedItem();
+    this.updateCurrentCollection();
+    this.activateItem();
+  }
 
+  updateCurrentCollection() {
+    itemCollections.forEach((collection, index) => {
+      if (collection.length == 0) {
+        itemCollections.splice(index, 1);
+        console.log("col was removed");
+      } else if (itemCollections.length == 1 && collection.length != 0) {
+        currentItemsCollection = collection.slice();
+        return;
+      }
+    });
+    currentItemsCollection = shuffle(itemCollections);
+    currentItemsCollection = currentItemsCollection[0].slice();
+  }
 
+  removeShowedItem() {
+    const result = {
+      col: -1,
+      row: -1,
+    };
+
+    itemCollections.forEach((row, index) => {
+      const itemPosition = row.indexOf(this.currentActiveItem);
+      if (itemPosition > -1) {
+        result.row = index;
+        result.col = itemPosition;
+      }
+    });
+
+    console.log("ItemPos: " + result.col, ", " + result.row);
+    itemCollections[result.row].splice(result.col, 1);
+  }
 }
